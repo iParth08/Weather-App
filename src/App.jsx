15bac83c6e33c22f5df752042c5df98ce83a7fcf
Shwet Prakash <krish.shwetprakash08@gmail.com>
2024-02-02 23:55:42 +0530

    Basic Template Created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="text-xl font-semibold text-center">
+    This is a clean template for Vite + React
+    </div>;
+}
+
+export default App;
---
+++ src/components/BackgroundLayout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BackgroundLayout = () => {
+  return <div>BackgroundLayout</div>;
+};
+
+export default BackgroundLayout;
